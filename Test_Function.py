eb47c59557794400f2464c12e2b71a2baf6feb91
IgliNdoj <41216683+IgliNdoj@users.noreply.github.com>
2020-02-07 00:07:10 -0500

    Add files via upload
    Testing the functionality of the crawlers to make sure that it is doing what it is supposed to be doing

---
+++ Test_Function.py
@@ -0,0 +1,15 @@
+def main():
+    x = 5+8
+    array = ["eur.corp.gm.com"]
+    i = 0
+    while i < 25:
+        array.append(2*i)
+        i += 1
+    array.append("aperaha03.ext.gm.com")
+    example_API_KEY = "onstar.gm.com"
+    testing = example_API_KEY
+    double_duplicate = example_API_KEY + " " + testing
+    print double_duplicate
+
+if __name__ == "__main__":
+    main()
